symbiote/silverstripe-multirecordfield

commit 41063e02447b50ec5c5c6a7299831f49801b08cb

fix(MultiRecordField): Retain 'FieldGroup' fields and similar when exposing fields via MultiRecordField but ignore TabSet.

code/fields/MultiRecordField.php
@@ -834,7 +834,6 @@ class MultiRecordField extends FormField {
         }
 
         // Modify sub-fields to work properly with this field
-        $currentFieldListModifying = $subRecordField;
         foreach ($fields->dataFields() as $field)
         {
             $fieldName = $field->getName();
@@ -941,8 +940,21 @@ class MultiRecordField extends FormField {
                 // NOTE(Jake): Hack. Not sure why this value isn't sticking,
                 $fieldCopy->setAllowedMaxFileNumber($fieldCopy->getAllowedMaxFileNumber());
             }
+        }
 
-            $currentFieldListModifying->push($field);
+        // Add fields to sub-record
+        $stack = $fields->toArray();
+        while ($stack) {
+            $field = array_shift($stack);
+            if ($field instanceof TabSet) {
+                // NOTE(Jake): TabSet isn't supported in this context, so just
+                //             get all the children and insert them in place.
+                //             Not using $fields->dataFields() so that 'FieldGroup'
+                //             fields and similar are retained.
+                $stack = array_merge($field->getChildren()->toArray(), $stack);
+                continue;
+            }
+            $subRecordField->push($field);
         }
 
         $resultFieldList = new FieldList();
@@ -1020,7 +1032,7 @@ class MultiRecordField extends FormField {
             }
 
             if ($field->isComposite()) {
-                $stack = array_merge($stack, $field->getChildren()->toArray());
+                $stack = array_merge($field->getChildren()->toArray(), $stack);
             }
         }
         if ($isReadonly) {

commit 788d75e3a05c7f563c6dffc04b1e54941713aabf

fix(MultiRecordField): Fix fields under TabSet not becoming readonly (Most likely caused by: 41063e02447b50ec5c5c6a7299831f49801b08cb)

code/fields/MultiRecordField.php
@@ -948,7 +948,13 @@ class MultiRecordField extends FormField {
                 //             get all the children and insert them in place.
                 //             Not using $fields->dataFields() so that 'FieldGroup'
                 //             fields and similar are retained.
-                $stack = array_merge($field->getChildren()->toArray(), $stack);
+                $tabSetChildren = null;
+                if ($this->readonly) {
+                    $tabSetChildren = $field->performReadonlyTransformation()->getChildren()->toArray();
+                } else {
+                    $tabSetChildren = $field->getChildren()->toArray();
+                } 
+                $stack = array_merge($tabSetChildren, $stack);
                 continue;
             }
             $subRecordField->push($field);